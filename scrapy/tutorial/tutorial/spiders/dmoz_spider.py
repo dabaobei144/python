0dd2eeaebbd7fce23815957118bdfe5acf3399a2
dabaobei144 <672528926@qq.com>
2015-12-01 23:17:39 +0800

    scrapy 递归爬步行街100页所有的帖子

--- scrapy/tutorial/tutorial/spiders/dmoz_spider.py
+++ scrapy/tutorial/tutorial/spiders/dmoz_spider.py
@@ -11,21 +11,26 @@ class DmozSpider(scrapy.Spider):
     start_urls = [
         domain + "/bxj/",
     ]
+    # self define
+    page_num = 0
+    def __init__(self):
+      print 1
+      self.file = open('records', 'w')
+    def __del__(self):
+      print 2
+      self.file.close()
     def parse(self, response):
-        page_num = 1
-        sel = Selector(response)
-        #items = sel.xpath('//td/a/text()').extract()
-        #for item in items:
-        #  print str(page_num) + ':\n' + item.encode('utf-8')
-        #  print '\n'
-        next_page = sel.xpath('//a[@class="next"]/@href').extract()
-        full_url = response.urljoin(next_page[0])
+        full_url = self.start_urls[0]
         yield scrapy.Request(full_url, callback=self.parse_question)
     def parse_question(self, response):
+        if self.page_num == 100:
+          return
+        self.page_num += 1
         sel = Selector(response)
         items = sel.xpath('//td/a/text()').extract()
         for item in items:
           print item.encode('utf-8')
+          self.file.write(item.encode('utf-8') + '\n')
         next_page = sel.xpath('//a[@class="next"]/@href').extract()
         full_url = response.urljoin(next_page[0])
         yield scrapy.Request(full_url, callback=self.parse_question)
