c3fafc985d6792370a1200f7e5311ae80a495cf1
dabaobei144 <672528926@qq.com>
2015-12-01 22:56:05 +0800

    scrapy 第一个能递归访问下一页的爬虫demo

--- scrapy/tutorial/tutorial/spiders/dmoz_spider.py
+++ scrapy/tutorial/tutorial/spiders/dmoz_spider.py
@@ -3,15 +3,29 @@
 import scrapy
 from scrapy.selector import Selector
 
+domain = "http://bbs.hupu.com"
+
 class DmozSpider(scrapy.Spider):
     name = "dmoz"
     allowed_domains = ["hupu.com"]
     start_urls = [
-        "http://bbs.hupu.com/bxj/",
+        domain + "/bxj/",
     ]
     def parse(self, response):
+        page_num = 1
+        sel = Selector(response)
+        #items = sel.xpath('//td/a/text()').extract()
+        #for item in items:
+        #  print str(page_num) + ':\n' + item.encode('utf-8')
+        #  print '\n'
+        next_page = sel.xpath('//a[@class="next"]/@href').extract()
+        full_url = response.urljoin(next_page[0])
+        yield scrapy.Request(full_url, callback=self.parse_question)
+    def parse_question(self, response):
         sel = Selector(response)
         items = sel.xpath('//td/a/text()').extract()
         for item in items:
           print item.encode('utf-8')
-        #print [item for item in sel.xpath('//a')]
+        next_page = sel.xpath('//a[@class="next"]/@href').extract()
+        full_url = response.urljoin(next_page[0])
+        yield scrapy.Request(full_url, callback=self.parse_question)
