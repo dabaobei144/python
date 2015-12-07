e926a38b3dcf25a6d9d42ea7bd0e56c32bc2799f
dabaobei144 <672528926@qq.com>
2015-12-07 23:54:17 +0800

    实现抓取单个帖子指定作者的所有楼层内容

--- scrapy/tutorial/tutorial/spiders/dmoz_spider.py
+++ scrapy/tutorial/tutorial/spiders/dmoz_spider.py
@@ -4,13 +4,14 @@ import scrapy
 from scrapy.selector import Selector
 import re
 
-domain = "http://bbs.hupu.com"
+domain = 'http://bbs.hupu.com'
+section = 'bxj'
 
 class DmozSpider(scrapy.Spider):
-    name = "dmoz"
-    allowed_domains = ["hupu.com"]
+    name = 'dmoz'
+    allowed_domains = ['hupu.com']
     start_urls = [
-        domain + "/bxj",
+        domain + '/' + section,
     ]
     # self define
     page_num = 0
@@ -24,22 +25,43 @@ class DmozSpider(scrapy.Spider):
         full_url = self.start_urls[0]
         yield scrapy.Request(full_url, callback=self.parse_question)
     def parse_question(self, response):
-        if self.page_num == 100:
+        if self.page_num == 1:
           return
         self.page_num += 1
-        for sel in response.xpath('//td[@class="p_title"]/a'):
-           #if len(sel.xpath('@href').re('/\d+.html')) != 0:
-           titles = sel.xpath('text()').extract()
-           links =  sel.xpath('@href').extract()
-           if len(titles) != 1:
-             continue
-           title = titles[0].encode('utf-8')
-           if re.search(r'不吹不黑', title) is not None:
-             print title
-
-           link = domain + links[0]
-           self.file.write(title + '\n')
-           self.file.write(link + '\n')
+        yield scrapy.Request('http://bbs.hupu.com/14535249.html', callback=self.parse_content)
+        #for sel in response.xpath('//td[@class="p_title"]/a'):
+        #   titles = sel.xpath('text()').extract()
+        #   links =  sel.xpath('@href').extract()
+        #   #if len(titles) != 1:
+        #   if not titles:
+        #     continue
+        #   title = titles[0].encode('utf-8')
+        #   #if re.search(r'专科', title) is None:
+        #   #  continue
+        #   link = domain + links[0]
+        #   #self.file.write(title + '\n')
+        #   #self.file.write(link + '\n')
+        #   yield scrapy.Request(link, callback=self.parse_content)
+        #next_page = response.xpath('//a[@class="next"]/@href').extract()
+        #full_url = response.urljoin(next_page[0])
+        #yield scrapy.Request(full_url, callback=self.parse_question)
+    # content
+    def parse_content(self, response):
+        #for sel in response.xpath('//div[@class="author"]/div[@class="left"]/a'):
+        for sel in response.xpath('//div[@class="floor_box"]'):
+          author = sel.xpath('.//div[@class="author"]/div[@class="left"]/a/text()').extract_first().encode('utf-8')
+          if not re.search(r'老九', author):
+            continue
+          contents = sel.xpath('.//tr/td/text()').extract()
+          for index, content in enumerate(contents):
+            print index, content.encode('utf-8')
+            self.write(content.encode('utf-8'))
         next_page = response.xpath('//a[@class="next"]/@href').extract()
+        if not next_page:
+          return
         full_url = response.urljoin(next_page[0])
-        yield scrapy.Request(full_url, callback=self.parse_question)
+        yield scrapy.Request(full_url, callback=self.parse_content)
+
+    def write(self, content):
+        content = content.replace('\n','')
+        self.file.write(content + '\n')
